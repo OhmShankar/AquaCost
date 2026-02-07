fb3234ee6c1e4ac63e5b2e8789a23a1b0099c68f
OhmShankar <ohmshankar722@gmail.com>
2026-02-08 00:59:44 +0530

    Update URL to localhost:8081

--- App.js
+++ App.js
@@ -8,7 +8,7 @@ export default function App() {
   // On web, redirect to the actual website
   if (Platform.OS === 'web') {
     if (typeof window !== 'undefined') {
-      window.location.href = 'http://localhost:3000';
+      window.location.href = 'http://localhost:8081';
     }
     return null;
   }
@@ -16,7 +16,7 @@ export default function App() {
   // On iOS/Android, use WebView
   return (
     <SafeAreaView style={{ flex: 1 }}>
-      <WebView source={{ uri: 'http://localhost:3000' }} />
+      <WebView source={{ uri: 'http://localhost:8081' }} />
     </SafeAreaView>
   );
 }
